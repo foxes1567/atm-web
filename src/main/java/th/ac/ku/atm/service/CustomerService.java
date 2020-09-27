769f5cc0a0c9bcc2aedf6ad7651786e45ffe3ba2
foxes1567 <fox_na_za@hotmail.com>
2020-09-27 23:03:28 +0700

    seond commit

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -2,6 +2,7 @@ package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
@@ -11,31 +12,28 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    private CustomerRepository repository;
 
-    @PostConstruct
-    public void postConstruct(){
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
+
 
     public void createCustomer(Customer customer){
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers(){
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     public Customer findCustomer(int id){
-        for(Customer customer: customerList){
-            if(customer.getId() == id){
-                return customer;
-            }
-        }
-        return null;
+        return repository.findById(id);
+
     }
+
 
     public Customer checkPin(Customer inputCustomer){
         Customer storedCustomer = findCustomer(inputCustomer.getId());
